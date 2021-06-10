dad2733bb0a05cfa6253b20e3eea6534e4ec1c60
VinZ83F <48916023+VinZ83F@users.noreply.github.com>
2021-06-10 15:53:03 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 * | Function    :   Contorl 1.8inch lcd Show (only CAPS and Numbers)
 * | Info        :
 *----------------
-* | This version:   V2.0.2
+* | This version:   V2.0.2b
 * | Date        :   2021-06-10
 * | Info        :   for micro:bit v2
 *
